2309b48a0cb7a1fdd7aba8a7929a47588292f417
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-12 09:00:15 -0500

    First exercise cpp

---
+++ Section 3/e1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	float num = 0.0f; 
+	int counter = 0;
+	std::cout << "Please enter a number between 1-10: ";
+	std::cin >> num;
+	while (!std::cin.good() || num < 1 || num>9 || (num - floor(num))) { //This checks if the number is an integer in the given range
+		std::cin.clear();
+		std::cout << "Please enter a number between 1-10: ";
+		std::cin >> num;
+		counter++;
+	}
+	std::string response = (counter > 6) ? "\nJesus goddamn christ, what on earth took you so long to simply get a number like "
+		: "\nThat is correct, you succesfully entered: ";
+	std::cout << response << num << "\n";
+}
