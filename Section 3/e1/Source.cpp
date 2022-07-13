066b94782e3784aeabd48aea53fe45605a8c03e0
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-13 10:24:48 -0500

    whoops

--- Section 3/e1/Source.cpp
+++ Section 3/e1/Source.cpp
@@ -5,17 +5,17 @@
 //Hint: We haven’t seen anything about input validation, this is a good chance to look up something you don’t know by yourself.See where google takes you.
 
 int main() {
-	float num = 0.0f; 
+	float number = 0.0f; 
 	int counter = 0;
 	std::cout << "Please enter a number between 1-10: ";
-	std::cin >> num;
-	while (!std::cin.good() || num < 1 || num>9 || (num - floor(num)!=0.0f)) { //This checks if the number is an integer in the given range
+	std::cin >> number;
+	while (!std::cin.good() || number < 1 || number>9 || (number - floor(number)!=0.0f)) { //This checks if the number is an integer in the given range
 		std::cin.clear();
 		std::cout << "Please enter a number between 1-10: ";
-		std::cin >> num;
+		std::cin >> number;
 		counter++;
 	}
 	std::string response = (counter > 6) ? "\nJesus goddamn christ, what on earth took you so long to simply get a number like "
 		: "\nThat is correct, you succesfully entered: ";
-	std::cout << response << num << "\n";
+	std::cout << response << number << "\n";
 }
